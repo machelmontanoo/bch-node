02cd4e36a318c1c7029152df7521c0ed5c80482f
machelmontanoo <91301142+machelmontanoo@users.noreply.github.com>
2025-11-16 23:25:18 -0500

    Create exports.sh

---
+++ apps/bch-node/exports.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export APP_BCH_RPC_USER="umbrel"
+export APP_BCH_RPC_PASSWORD="${APP_PASSWORD}"
+export APP_BCH_RPC_PORT="9332"
+
+export APP_BCH_NODE_IP="bch-node_node_1"
+
+export APP_BCH_P2P_PORT="9333"
